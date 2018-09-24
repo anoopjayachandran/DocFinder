9e3b7e6d5203282348be8e5423ba5ac16b7dbc21
anoop.jayachandran <anoop.jayachandran@infospica.com>
2018-09-24 14:54:08 +0530

    SystemConstant added

---
+++ DocFinder/src/main/java/com/sargam/docfinder/SystemConstant.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sargam.docfinder;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Anoop Jayachandran
+ */
+public class SystemConstant {
+
+  public static final String DOC_CONTENTS = "contents";
+  public static final String DOC_NAME = "filename";
+  public static final String DOC_PATH = "filepath";
+  public static final String SKILL = "skills";
+  public static final int MAX_SEARCH = 10;
+
+  public static final List<String> SKILLS = Arrays.asList("java", "jsf", "core java", "jsp", "servlet", "jpa", "eclipselink", "hibernate", "spring", "c#", "php", "vb.net", "asp.net");
+
+}
