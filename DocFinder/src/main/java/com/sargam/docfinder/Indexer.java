017a7b65a24cae58c21bbd6d9d4cc3b0bd00dd39
anoop.jayachandran <anoop.jayachandran@infospica.com>
2018-09-24 14:56:08 +0530

    Skills and file name field added to document

--- DocFinder/src/main/java/com/sargam/docfinder/Indexer.java
+++ DocFinder/src/main/java/com/sargam/docfinder/Indexer.java
@@ -18,6 +18,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.lucene.analysis.Analyzer;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -29,9 +31,12 @@ import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 
 /**
@@ -40,65 +45,107 @@ import org.apache.tika.metadata.Metadata;
  */
 public class Indexer {
 
-    private IndexWriter writer;
+  private IndexWriter writer;
+  private String indexDirectoryPath;
 
-    public Indexer(String indexDirectoryPath) throws IOException {
-        //this directory will contain the indexes
-        Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
+  public Indexer(String indexDirectoryPath) throws IOException {
+    this.indexDirectoryPath = indexDirectoryPath;
+  }
 
-        //create the indexer
-        Analyzer analyzer = new StandardAnalyzer();
-        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
-        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-        writer = new IndexWriter(indexDirectory, iwc);
+  private IndexWriter getIndexWriter() throws IOException {
+    if (writer == null) {
+      //this directory will contain the indexes
+      Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
 
+      //create the indexer
+      Analyzer analyzer = new StandardAnalyzer();
+      IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+      iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+      writer = new IndexWriter(indexDirectory, iwc);
     }
+    return writer;
+  }
 
-    public void close() throws CorruptIndexException, IOException {
-        writer.close();
+  public void close() throws CorruptIndexException, IOException {
+    getIndexWriter().close();
+  }
+
+  private Document getDocument(Path file) throws IOException {
+    Document document = new Document();
+
+    Metadata metadata = new Metadata();
+    StringBuilder fileContent = null;
+    try {
+      fileContent = new StringBuilder(new Tika().parseToString(file));
+    } catch (TikaException ex) {
+      Logger.getLogger(Indexer.class.getName()).log(Level.SEVERE, null, ex);
     }
+    Reader content = new Tika().parse(new FileInputStream(file.toFile()), metadata);
 
-    private Document getDocument(Path file) throws IOException {
-        Document document = new Document();
+    //index file contents
+    //  Field contentField = new Field(LuceneConstants.CONTENTS, new FileReader(file));
+    //InputStream stream = Files.newInputStream(file);
+    //document.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+    document.add(new TextField(SystemConstant.DOC_CONTENTS, content));
 
-        Metadata metadata = new Metadata();
-        Reader content = new Tika().parse(new FileInputStream(file.toFile()), metadata);
-        
-        //index file contents
-        //  Field contentField = new Field(LuceneConstants.CONTENTS, new FileReader(file));
-        //InputStream stream = Files.newInputStream(file);
-        //document.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
-        
-        document.add(new TextField("contents", content));
-        //index file name
-        System.out.println(file.toString());
-        Field filePathField = new StringField(LuceneConstants.FILE_PATH, file.toString(), Field.Store.YES);
+    //index file name    
+    Field filePathField = new StringField(SystemConstant.DOC_PATH, file.toString(), Field.Store.YES);
+    document.add(filePathField);
 
-        document.add(filePathField);
+    Field fileNameField = new TextField(SystemConstant.DOC_NAME, file.getFileName().toString().toLowerCase(), Field.Store.YES);
+    document.add(fileNameField);
 
-        return document;
+    document.add(new TextField(SystemConstant.SKILL, getSkills(fileContent.toString().toLowerCase()), Field.Store.YES));
+
+    return document;
+  }
+
+  private void indexFile(File file) throws IOException, ParseException {
+    System.out.println("Indexing " + file.getName());
+    Document document = getDocument(Paths.get(file.getCanonicalPath()));    
+    getIndexWriter().addDocument(document);
+  }
+
+  public int createIndex(String dataDirPath, FileFilter filter)
+          throws IOException, ParseException {
+    //get all files in the data directory
+    File[] files = new File(dataDirPath).listFiles();
+    //getIndexWriter().deleteAll();
+    for (File file : files) {
+      if (!file.isDirectory()
+              && !file.isHidden()
+              && file.exists()
+              && file.canRead()
+              && filter.accept(file)) {
+        indexFile(file);
+      }
     }
+    int indexedCount = getIndexWriter().numDocs();
+    close();
+    return indexedCount;
+  }
 
-    private void indexFile(File file) throws IOException {
-        System.out.println("Indexing " + file.getCanonicalPath());
-        Document document = getDocument(Paths.get(file.getCanonicalPath()));
-        writer.addDocument(document);
-    }
-
-    public int createIndex(String dataDirPath, FileFilter filter)
-            throws IOException {
-        //get all files in the data directory
-        File[] files = new File(dataDirPath).listFiles();
-
-        for (File file : files) {
-            if (!file.isDirectory()
-                    && !file.isHidden()
-                    && file.exists()
-                    && file.canRead()
-                    && filter.accept(file)) {
-                indexFile(file);
-            }
+  private String getSkills(String content) {
+    StringBuilder skills = new StringBuilder();
+    if (content != null) {
+      for (String skill : SystemConstant.SKILLS) {
+        if (content.contains(skill)) {
+          if (skills.length() == 0) {
+            skills.append(skill);
+          } else {
+            skills.append(" ").append(skill);
+          }
         }
-        return writer.numDocs();
+      }
     }
+    return skills.toString();
+  }
+
+  private void removeDocument(Path path) {
+
+  }
+
+  private void updateDocument(Path path) {
+
+  }
 }
